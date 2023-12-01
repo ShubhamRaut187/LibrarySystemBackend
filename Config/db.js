df910efa56a309d0fb33e54adeda8e31cc24996d
ShubhamRaut187 <raut.shubham4554@gmail.com>
2023-12-02 04:31:28 +0530

    Backend Completed

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongodb = process.env.MONGODB || 'mongodb+srv://librarysystem187:5cjhubPMsskAyBmW@cluster0.lbi1gs6.mongodb.net/BookLibrarySystem'
+const mongodb = process.env.MONGODB;
 
 const connection = mongoose.connect(mongodb);
 module.exports={
